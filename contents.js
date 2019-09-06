acec5e445b5abeda83caad677b09c1cdcd2891dc
Sinevia <info@sinevia.com>
2019-09-06 06:17:08 +0000

    Update

--- contents.js
+++ contents.js
@@ -1,6 +1,6 @@
 var Contents = {
     $type:"div",
     id:"Contents",
-    style:"min-height:200px;padding:40px 20px;",
+    style:"min-height:500px;padding:0px 20px;",
     $text:"Loading contents..."
 };
